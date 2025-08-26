e0baa312da0210345f6146b7d7dfcd047f0155d8
chongyock-eng-asus <Chongyock_Eng@asus.com>
2025-08-26 11:44:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 def get_config():
     return {
         "batch_size": 8,
-        "num_epochs": 20,
+        "num_epochs": 1,
         "lr": 10**-4,
         "model_basename": "meta-llama/Llama-3.2-1B",
-        "num_masks": 3,
-        "max_length": 20,
+        "num_masks": 8,
+        "max_length": 512,
         "API_KEY":"",
         "lora_rank": 128, 
         "lora_alpha":256,
-        "dataset_size": 5000,
+        "dataset_size": '',
         "datasource":"allenai/tulu-3-sft-mixture",
     }
