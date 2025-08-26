cbd2f1e70a0b217ff39bd241aea267e7f3edf857
chongyock-eng-asus <Chongyock_Eng@asus.com>
2025-08-26 11:55:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def get_config():
         "lr": 10**-4,
         "model_basename": "meta-llama/Llama-3.2-1B",
         "num_masks": 8,
-        "max_length": 512,
+        "max_length": 1024,
         "API_KEY":"",
         "lora_rank": 128, 
         "lora_alpha":256,
